90dd76a028b01f634e053d74a6d4bb3f86e93467
Clecio Batista Santos <cleciosan@hotmail.com>
2023-03-24 17:49:47 -0300

    Montando cabelalho da pagina

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className="n-wrapper">
+            <div className="n-left">
+                <div className="n-name">Clécio</div>
+                <span>toggle</span>
+            </div>
+
+            <div className="n-right">
+                <div className="n-list">
+                    <ul>
+                        <li>Principal</li>
+                        <li>Serviços</li>
+                        <li>Experiência</li>
+                        <li>Portfolio</li>
+                        <li>Depoimentos</li>
+                    </ul>
+                </div>
+                <button className="button">
+                    Contate-nos!
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
