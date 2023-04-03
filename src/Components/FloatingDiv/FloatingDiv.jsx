65bb497bb20448866c1fe4d5352909a9062a587c
Clecio Batista Santos <cleciosan@hotmail.com>
2023-04-03 13:31:00 -0300

    Implementação do lado direito da página, imagens e animações

---
+++ src/Components/FloatingDiv/FloatingDiv.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FloatingDiv = () => {
+    return (
+        <div className="floatingdiv">
+
+        </div>
+    )
+}
+
+export default FloatingDiv
--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import './Intro.css'
+import Vector1 from '../../img/Vector1.png';
+import Vector2 from '../../img/Vector2.png';
+import boy from '../../img/boy.png';
+import clecio from '../../img/clecio.png';
+import thumbup from '../../img/thumbup.png';
+import Crown from '../../img/crown.png';
+import glassesimoji from '../../img/glassesimoji.png';
+
 import Github from '../../img/github.png';
 import LinkedIn from '../../img/linkedin.png';
 import Instagram from '../../img/instagram.png';
+
 
 const Intro = () => {
     return (
@@ -12,28 +21,33 @@ const Intro = () => {
                     <span>Olá! Sou</span>
                     <span>Clécio Batista</span>
                     <span>
-                        Desenvolvedor Frontend com alto
-                        nível de experiência em design e
-                        desenvolvimento web, produzindo
-                        Trabalho de Qualidade.
+                        Desenvolvedor Frontend com alto nível 
+                        de experiência em design e desenvolvimento web, 
+                        produzindo Trabalho de Qualidade.
                     </span>
                 </div>
 
                 <button className="button i-button">Contrate-me</button>
+                
                 <div className='i-icons'>
-                    <img src={Github} alt=""/>
-                    <img src={LinkedIn} alt=""/>
-                    <img src={Instagram} alt=""/>
+                    <img src={Github} alt="" />
+                    <img src={LinkedIn} alt="" />
+                    <img src={Instagram} alt="" />
                 </div>
 
             </div>
 
-
-            <div className="i-right">
-                <div className="i-name">
-                    <span>Lado Direito</span>
+            <div>
+                <div className="i-right">
+                    <img src={Vector1} alt=''/>
+                    <img src={Vector2} alt=''/>
+                    <img src={boy} alt=''/>
+                    <div>
+                        <floatingDiv></floatingDiv>
+                    </div>
                 </div>
             </div>
+
         </div>
     )
 }
